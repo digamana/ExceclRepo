d975f3689f49ffd37df5c27fc5b447ccd02b6ebc
q12389 <q12389@yahoo.com.tw>
2022-11-27 23:53:14 +0800

    OK

--- ExceclConsole/ExceclConsole/Program.cs
+++ ExceclConsole/ExceclConsole/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OfficeOpenXml.Style;
+using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OfficeOpenXml.DataValidation;
 
 namespace ExceclConsole
 {
@@ -10,14 +14,38 @@ namespace ExceclConsole
     {
         static void Main(string[] args)
         {
-            /*
-            將各個讀寫Excel套件的用法簡單整理出來
-            詳見分支
-            IronXL
-            Microsoft.Office.Interop.Excel
-            ExcelLibrary 
-            freespire.xls
-             */
+            Excel excel = new Excel(@"File Path", "SheetName");
+            var Result = excel.sheet.Cells[3, 3].Value;
+        }
+    }
+    public class Excel
+    {
+        public string FilePath { get; private set; }
+        public string SheetName { get; private set; }
+        public ExcelPackage workbook   { get; private set; }
+        public ExcelWorksheet sheet { get; private set; }
+        
+        public Excel(string filePath, string sheetName)
+        {
+            FilePath = filePath;
+            SheetName = sheetName;
+            ini();
+        }
+
+        public ExcelWorksheet SetSheet(string sheetName)
+        {
+            sheet = workbook.Workbook.Worksheets[sheetName]; // 可以使用頁籤名稱
+            return sheet;
+        }
+        public void ini()
+        {
+            workbook = new ExcelPackage(new FileInfo(FilePath));
+            sheet =workbook.Workbook.Worksheets[SheetName];
+        }
+        public void Dispose() 
+        {
+            workbook.Dispose();
+            sheet.Dispose();
         }
     }
 }
